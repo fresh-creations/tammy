406390ddb819943676ad1d8cb8fe2e2f5b7a41f1
jonasdoevenspeck <jonasdoevenspeck@gmail.com>
2022-12-29 15:58:06 +0100

    add moviepy and sample (#5)
    * add moviepy and sample

--- run_tammy.py
+++ run_tammy.py
@@ -3,6 +3,7 @@ import torch
 import yaml
 import argparse
 import warnings
+from moviepy.editor import VideoFileClip, AudioFileClip
 from tammy.sequence_maker import SequenceMaker
 from tammy.prompthandler import PromptHandler
 from tammy.upscaling.super_resolution import Upscaler
@@ -89,3 +90,14 @@ if do_slowmo:
         source_dir = step_dir
     motion_slower = MotionSlower(slowmo_settings, device, batch_size=1)
     motion_slower.slomo(source_dir, video_name)
+
+# merge the video and audio
+if do_slowmo:
+    generated_seconds = ((sequence_settings['max_frames']-2)*slowmo_settings['slowmo_factor'])/slowmo_settings['target_fps']
+else:
+    generated_seconds = (sequence_settings['max_frames']-2)/sequence_settings['initital_fps']
+video_clip = VideoFileClip(video_name)
+audio_clip = AudioFileClip('thoughtsarebeings_clip.wav')
+audio_clip = audio_clip.subclip(t_start=0, t_end=generated_seconds)
+final_clip = video_clip.set_audio(audio_clip)
+final_clip.write_videofile(os.path.join(exp_dir,'video_with_audio.mp4'))
